4f8d6f0ca83e224a301e25792cd5d2c3d5b1e80d
YOUR NAME <YOUR@EMAIL.com>
2020-01-03 21:45:17 +0000

    Updates

---
+++ precache-manifest.b4c595460eb6015be0edece1a5b7ec6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ae935b346e2b9be2f2331f55b9856a",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "3f99b7d66fd6f2152d7d",
+    "url": "/monsters/static/css/main.a1ce2f30.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f99b7d66fd6f2152d7d",
+    "url": "/monsters/static/js/main.64dcfd2c.chunk.js"
+  },
+  {
+    "revision": "475d84c361081acc4a17",
+    "url": "/monsters/static/js/runtime-main.05e5ea3c.js"
+  }
+]);
